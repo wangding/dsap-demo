30d1fe63c71c7514e5a59e5c5bcbd9bd8ab236fe
wangding <wangding@onest.net>
2025-07-03 11:47:31 +0800

    modify code all_demo/ch03_analysis

--- all_demo/ch03_analysis/14_number_game.py
+++ all_demo/ch03_analysis/14_number_game.py
@@ -8,14 +8,16 @@ from random import randint
 def game():
   answer = randint(1, 100)
   print("猜数游戏，现在开始...")
+  n = 0
   while True:
     guess = randint(1, 100)
+    n += 1
     if guess == answer:
       print(f"\n太棒了！这个数就是 {answer}")
       break
     elif guess < answer:
-      print(f'{guess:2} 不对。猜的数小了。')
+      print(f'{n:2}: {guess:2} 不对。猜的数小了。')
     else:
-      print(f'{guess:2} 不对。猜的数大了。')
+      print(f'{n:2}: {guess:2} 不对。猜的数大了。')
 
 game()
--- all_demo/ch03_analysis/15_number_game2.py
+++ all_demo/ch03_analysis/15_number_game2.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 
 # 猜数游戏
-# 模拟策略三：随机查找，避免重复
+# 模拟策略三：随机查找，缩小范围
 
 from random import randint
 
 class Numbers():
   def __init__(self, start=1, end=100):
     self.__nums = list(range(start, end + 1))
+    self.__low  = start
+    self.__high = end
 
   def get(self):
-    """获取一个随机数，并从列表中删除它"""
-    index = randint(0, len(self.__nums) - 1)
-    num = self.__nums[index]
-    self.__nums[index] = self.__nums[-1]  # 用最后一个数替换
-    self.__nums.pop()  # 删除最后一个数
-    return num
-
-  def __repr__(self): return str(self.__nums)
+    index = randint(0, self.__high - self.__low)
+    nums = self.__nums[self.__low-1: self.__high]
+    return nums[index]
+  
+  def set_low(self, low):   self.__low  = low
+  def set_high(self, high): self.__high = high
+  def __repr__(self):
+    return 'low:  ' + str(self.__low) + '\n' + \
+           'high: ' + str(self.__high) + '\n' + \
+           'nums: ' + str(self.__nums[self.__low-1: self.__high])
 
 def game():
   answer = randint(1, 100)
@@ -32,7 +36,9 @@ def game():
       break
     elif guess < answer:
       print(f'{n:2}: {guess:2} 不对。猜的数小了。')
+      nums.set_low(guess+1)
     else:
       print(f'{n:2}: {guess:2} 不对。猜的数大了。')
+      nums.set_high(guess-1)
 
 game()
